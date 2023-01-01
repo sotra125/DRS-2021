6b8bbc4f3fe77b5cc4d3f181341e9dd88a93b1a7
Uros1235 <101191802+Uros1235@users.noreply.github.com>
2023-01-01 20:26:19 +0100

    made database models

---
+++ models/user.py
@@ -0,0 +1,14 @@
+from application_data import db
+
+
+class User(db.Model):
+    user_id = db.Column(db.String, primary_key=True)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    city = db.Column(db.String, nullable=False)
+    country = db.Column(db.String, nullable=False)
+    phone_number = db.Column(db.String, nullable=False)
+    is_verified = db.Column(db.Boolean, nullable=False)
